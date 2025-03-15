27fda1ad506b216aac5b98e1cb00d24cd10baf8b
Danielzl12 <danielzuluagac06@gmail.com>
2025-03-15 14:14:47 -0500

    switch_case

---
+++ Guia_3/semana_alternativa.py
@@ -0,0 +1,21 @@
+def switch_case ():
+    dia=float(input("Digíte numero del dia: "))
+    match dia:
+        case 1:
+            print("Lunes")
+        case 2:
+            print("Martes")
+        case 3:
+            print("Miércoles")
+        case 4:
+            print("Jueves")
+        case 5:
+            print("Viernes")
+        case 6:
+            print("Sábado")
+        case 7:
+            print("Domingo")
+        case _:
+            print("Dia invalido")
+
+switch_case()
