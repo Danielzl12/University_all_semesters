adc8e81b8d034924655c643fdf99e9f2608148e6
Danielzl12 <danielzuluagac06@gmail.com>
2025-03-15 14:25:58 -0500

    iteración

--- Guia_5/patron_asteriscos_iteración.py
+++ Guia_5/patron_asteriscos_iteración.py
@@ -1,5 +1,6 @@
 def patron(): 
     n = int(input("Digíte un numero: ")) 
     for i in range(n): 
+        # Se itera desde 0, por eso se pone el +1 para que la primera línea tenga 1 asterisco
         print("*"*(i+1)) 
-patron() 
+patron()
