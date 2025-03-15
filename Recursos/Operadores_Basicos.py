5833942be523e40cc3cab980251ed6974db25347
Danielzl12 <danielzuluagac06@gmail.com>
2025-03-15 18:06:44 -0500

    operadores

---
+++ Recursos/Operadores_Basicos.py
@@ -0,0 +1,11 @@
+#exponente
+print(2 ** 3)
+#division normal (siempre devuelve un float)
+print(6 / 3)
+#division entera (devuelve entero en caso tal que sea entero)
+print(6 // 3)
+#MODULO
+print(14 % 4)
+#14 // 4 da como resultado un 3 → esta es la parte entera, es decir el cociente;
+#3 * 4 da como resultado 12 → como resultado de la multiplicación entre el cociente y el divisor;
+#14 - 12 da como resultado 2 → este es el residuo.
