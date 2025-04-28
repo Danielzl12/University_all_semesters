9105a3b5bd6c60530872ace3342c313e309cbea3
Daniel Zuluaga <danielzuluagac06@gmail.com>
2025-04-28 16:56:48 -0500

    Matrices

--- Universidad/Primer_Semestre/Guia_7/Matrices_2/Elemento_Mayor/src/Main.java
+++ Universidad/Primer_Semestre/Guia_7/Matrices_2/Elemento_Mayor/src/Main.java
@@ -1,15 +1,37 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import javax.swing.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    int[][] matriz;
+    int mayor;
+
+    public Main() {
+        matriz = new int[3][3]; // Using 3x3 as an example size
+        // Fill the matrix with user input
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                String input = JOptionPane.showInputDialog(null, "Ingrese un numero para posición [" + i + "][" + j + "]");
+                matriz[i][j] = Integer.parseInt(input);
+            }
         }
+
+        // Initialize mayor with the first element
+        mayor = matriz[0][0];
+
+        // Find the largest element
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                if (matriz[i][j] > mayor) {
+                    mayor = matriz[i][j];
+                }
+            }
+        }
+
+        // Display the result once after finding the largest element
+        JOptionPane.showMessageDialog(null, "El mayor es: " + mayor);
     }
-}
+
+    public static void main(String[] args) {
+        Main m = new Main();
+    }
+    }
